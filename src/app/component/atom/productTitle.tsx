287e46dc9aa3c18ed8d27c0a6f0e2d0a5616420e
leakna <phornleakna@gmail.com>
2024-08-21 01:18:56 +0700

    update

--- src/app/component/atom/productTitle.tsx
+++ src/app/component/atom/productTitle.tsx
@@ -1,5 +1,5 @@
 const ProductTitle = ({ title }: { title: string }) => (
-  <div className="font-bold">{title}</div>
+  <div className="font-bold">{title} .</div>
 );
 
 export default ProductTitle;
